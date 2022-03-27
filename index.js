7a7459b642f17f15f022a719948cc01b6ab8e892
Prakul Mangla <94923215+mprakul@users.noreply.github.com>
2022-03-27 20:21:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,31 +3,31 @@ function playSound(pressedKey)
   switch(pressedKey)
   {
       case "w" :
-            var crash = new Audio("sounds/crash.mp3");
+            var crash = new Audio("crash.mp3");
             crash.play();
             break;
       case "a" :
-            var kick = new Audio("sounds/kick-bass.mp3");
+            var kick = new Audio("kick-bass.mp3");
             kick.play();
             break;
       case "s" :
-            var snare = new Audio("sounds/snare.mp3");
+            var snare = new Audio("snare.mp3");
             snare.play();
             break;
       case "d" :
-            var tom1 = new Audio("sounds/tom-1.mp3");
+            var tom1 = new Audio("tom-1.mp3");
             tom1.play();
             break;
       case "j" :
-            var tom2 = new Audio("sounds/tom-2.mp3");
+            var tom2 = new Audio("tom-2.mp3");
             tom2.play();
             break;
       case "k" :
-            var tom3 = new Audio("sounds/tom-3.mp3");
+            var tom3 = new Audio("tom-3.mp3");
             tom3.play();
             break;
       case "l" :
-            var tom4 = new Audio("sounds/tom-4.mp3");
+            var tom4 = new Audio("tom-4.mp3");
             tom4.play();
             break;
 
@@ -59,7 +59,6 @@ for(var i = 0 ; i < document.querySelectorAll(".drum").length ; i++)
 // to detect any button was pressed on website
 function clickOnButton()
 {
-//  alert("Clicked on Button");
   var buttonInnerHTML = this.innerHTML;
   playSound(buttonInnerHTML);
   buttonAnimation(buttonInnerHTML);
@@ -69,5 +68,4 @@ function clickOnButton()
 document.addEventListener("keydown", function(event){
        playSound(event.key);
        buttonAnimation(event.key);
-    //  alert("Key is Pressed!!");
   });
